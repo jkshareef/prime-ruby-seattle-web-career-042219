e88e44abb8603edd31a2991f94bef9af7faa65ad
Jamal Shareef <theepie@hotmail.com>
2019-04-02 03:15:43 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,10 @@
 # Add  code here!
 def prime?(integer)
-  for number in 1..integer
+  if integer < 0
+    integer = integer*-1
+  for number in 2..integer-1 do
+    if integer % number == 0
+      false
+    end
+    true
 end
