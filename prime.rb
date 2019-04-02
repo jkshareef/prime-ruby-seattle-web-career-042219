fb475d7558e2136f064a428650f24b9224e93283
Jamal Shareef <theepie@hotmail.com>
2019-04-02 03:19:06 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,8 +2,10 @@
 def prime?(integer)
   if integer < 0
     integer = integer*-1
+  end
   if interger == 2
     true
+  end
   for number in 2..integer-1 do
     if integer % number == 0
       false
